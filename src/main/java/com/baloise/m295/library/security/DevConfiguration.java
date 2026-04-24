7b148e35fd910e0253d5aa126f3839c78d086733
Julian Schiller <julian.schller@stud.aprentas.com>
2026-04-24 08:32:05 +0200

    add java doc and create erm picture

--- src/main/java/com/baloise/m295/library/security/DevConfiguration.java
+++ src/main/java/com/baloise/m295/library/security/DevConfiguration.java
@@ -6,15 +6,21 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * CORS Configuration class for security
+ * @author Julian Schiller
+ */
 @Configuration
 @Profile("development")
 public class DevConfiguration implements WebMvcConfigurer {
 
+    /** {@inheritDoc} */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/library").addResourceLocations("classpath:/public/");
     }
 
+    /** {@inheritDoc} */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
