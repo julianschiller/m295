b6f859962a935e0205f825244d156b843f81da9a
Julian Schiller <julian.schller@stud.aprentas.com>
2026-05-04 10:32:22 +0200

    simplified

--- src/main/java/com/baloise/m295/library/security/AdGroupToRoleMapper.java
+++ src/main/java/com/baloise/m295/library/security/AdGroupToRoleMapper.java
@@ -5,22 +5,19 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 
 public class AdGroupToRoleMapper implements GrantedAuthoritiesMapper {
 
-    private static final Set<String> RELEVANT_GROUPS = Set.of("USER", "LIBRARIAN");
+    private static final Set<String> RELEVANT_GROUPS = Set.of("ROLE_USER", "ROLE_LIBRARIAN");
 
     @Override
     public Collection<? extends GrantedAuthority> mapAuthorities(
             Collection<? extends GrantedAuthority> authorities) {
 
         return authorities.stream()
-                .map(GrantedAuthority::getAuthority)
-                .map(authority -> authority.replaceFirst("^ROLE_", ""))
-                .filter(RELEVANT_GROUPS::contains)
-                .map(groupName -> new SimpleGrantedAuthority("ROLE_" + groupName))
-                .collect(Collectors.toSet());
+            .filter(a -> RELEVANT_GROUPS.contains(a.getAuthority()))
+            .collect(Collectors.toSet());
+        
     }
 }
